e5b17896d0720fbdb90550c61019c736690035c8
gustavojcd <deep.bathory@gmail.com>
2022-08-15 20:31:34 -0500

    added context menu and deleted popup menu

--- background.js
+++ background.js
@@ -1,1 +1,65 @@
-console.log("hola mundo");
+chrome.contextMenus.create({
+    "title": "Guardar todos",
+    "contexts": ["all"],
+    "id": "allTabs",
+});
+
+chrome.contextMenus.create({
+    "title": "Guardar pestaña actual",
+    "contexts": ["all"],
+    "id": "activeTab"
+});
+
+chrome.contextMenus.create({
+    "title": "Guardar todas excepto la pestaña actual",
+    "contexts": ["all"],
+    "id": "allTabsExceptActive"
+});
+
+async function activeTab() {
+    let queryOptions = { active: true, lastFocusedWindow: true };
+    let [tab] = await chrome.tabs.query(queryOptions);
+    const { id, title, url } = tab;
+    createBookmark(title, url)
+    chrome.tabs.remove(id)
+}
+
+async function allTabs(queryOptions) {
+    let allTabs = chrome.tabs.query(queryOptions);
+    allTabs.then((tabs) => {
+        for (let tab of tabs) {
+            const { id, title, url } = tab;
+            createBookmark(title, url);
+            chrome.tabs.remove(id);
+        }
+    })
+}
+
+function createBookmark(title, url) {
+    chrome.bookmarks.create({
+        'title': title,
+        'url': url
+    })
+}
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+    if (info.menuItemId === 'allTabs') {
+        allTabs({ currentWindow: true });
+        chrome.tabs.create({
+            'url': 'chrome://bookmarks/?id=2'
+        })
+    }
+    if (info.menuItemId === 'activeTab') {
+        activeTab();
+    }
+    if (info.menuItemId === 'allTabsExceptActive') {
+        allTabs({ active: false });
+    }
+})
+
+chrome.action.onClicked.addListener(() => {
+    allTabs({ currentWindow: true });
+    chrome.tabs.create({
+        'url': 'chrome://bookmarks/?id=2'
+    })
+});
--- content.js
+++
@@ -1,1 +0,0 @@
-alert("marcador guardado");
