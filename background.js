647a65a9c77ea2773105f0d2a8fca00126fcfe69
gcharalla <g.charalla.dev@gmail.com>
2023-07-31 14:01:23 -0500

    save tabs to the right and left of the active tab

--- background.js
+++ background.js
@@ -1,39 +1,32 @@
 chrome.contextMenus.create({
-    "title": "Guardar todos",
+    "title": "Save all tabs",
     "contexts": ["all"],
     "id": "allTabs",
 });
 
 chrome.contextMenus.create({
-    "title": "Guardar pestaña actual",
+    "title": "Save active tab",
     "contexts": ["all"],
     "id": "activeTab"
 });
 
 chrome.contextMenus.create({
-    "title": "Guardar todas excepto la pestaña actual",
+    "title": "Save all except active tab",
     "contexts": ["all"],
     "id": "allTabsExceptActive"
 });
 
-async function activeTab() {
-    let queryOptions = { active: true, lastFocusedWindow: true };
-    let [tab] = await chrome.tabs.query(queryOptions);
-    const { id, title, url } = tab;
-    createBookmark(title, url)
-    chrome.tabs.remove(id);
-}
+chrome.contextMenus.create({
+    "title": "save all tabs to the right of the active tab",
+    "contexts": ["all"],
+    "id": "allTabsRigth"
+});
 
-async function allTabs(queryOptions) {
-    let allTabs = chrome.tabs.query(queryOptions);
-    allTabs.then((tabs) => {
-        for (let tab of tabs) {
-            const { id, title, url } = tab;
-            createBookmark(title, url);
-            chrome.tabs.remove(id);
-        }
-    })
-}
+chrome.contextMenus.create({
+    "title": "save all tabs to the left of the active tab",
+    "contexts": ["all"],
+    "id": "allTabsLeft"
+});
 
 function createBookmark(title, url) {
     chrome.bookmarks.create({
@@ -48,6 +41,44 @@ function callAdmin() {
     })
 }
 
+
+async function activeTab() {
+    let queryOptions = { active: true, lastFocusedWindow: true };
+    let [tab] = await chrome.tabs.query(queryOptions);
+    const { id, title, url } = tab;
+    createBookmark(title, url)
+    chrome.tabs.remove(id);
+}
+
+async function allTabs(queryOptions) {
+    let allTabs = await chrome.tabs.query(queryOptions);
+    allTabs.then((tabs) => {
+        for (let tab of tabs) {
+            const { id, title, url } = tab;
+            createBookmark(title, url);
+            chrome.tabs.remove(id);
+        }
+    })
+}
+
+async function saveAndCloseTabs(direction) {
+    let [currentTab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
+    let allTabs = await chrome.tabs.query({ currentWindow: true });
+
+    const activeTabIndex = allTabs.findIndex(tab => tab.id === currentTab.id);
+
+    if (direction === "right") {
+        tabsToSave = allTabs.slice(activeTabIndex + 1);
+    } else if (direction === "left") {
+        tabsToSave = allTabs.slice(0, activeTabIndex);
+    }
+
+    tabsToSave.forEach(tab => {
+        createBookmark(tab.title, tab.url);
+        chrome.tabs.remove(tab.id);
+    });
+}
+
 chrome.contextMenus.onClicked.addListener((info, tab) => {
     if (info.menuItemId === 'allTabs') {
         allTabs({ currentWindow: true });
@@ -58,6 +89,12 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
     }
     if (info.menuItemId === 'allTabsExceptActive') {
         allTabs({ active: false });
+    }
+    if (info.menuItemId === 'allTabsRigth') {
+        saveAndCloseTabs('right');
+    }
+    if (info.menuItemId === 'allTabsLeft') {
+        saveAndCloseTabs('left');
     }
 })
 
